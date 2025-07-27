b6673f969e17c1184735c49c9be2a22b4aae6fd0
Justus Kleine <justus.kleine@gmail.com>
2025-07-27 10:23:46 +0300

    overworlds map locations fixed

--- data/realms.js
+++ data/realms.js
@@ -12,7 +12,7 @@ export const REALMS = [
                 name: "Green Meadows",
                 mapImage: "images/backgrounds/sub_zone/map_meadows_zoomed.png",
                 monsterAreaBg: "images/backgrounds/bg_green_meadows.png",
-                coords: { top: '78%', left: '20%' },
+                coords: { top: '70%', left: '25%' },
                 icon: 'images/icons/green_meadows.png',
                 subZones: {
                     // --- Sub-Zone: Verdant Fields (Levels 1-24) ---
@@ -20,17 +20,17 @@ export const REALMS = [
                     // --- Sub-Zone: General's Outpost (Boss Lvl 25) ---
                     "general_outpost": { name: "General's Outpost", levelRange: [25, 25], monsterPool: [MONSTERS.SLIME_GENERAL], coords: {top: '60%', left: '25%'}, isBoss: true, icon: 'images/icons/general_outpost.png' },
                     // --- Sub-Zone: Whispering Woods (Levels 26-49) ---
-                    "whispering_woods": { name: "Whispering Woods", levelRange: [26, 49], monsterPool: [MONSTERS.WOLF, MONSTERS.BAT, MONSTERS.GIANT_SPIDER], coords: {top: '83%', left: '58%'}, icon: 'images/icons/whispering_woods.png' },
+                    "whispering_woods": { name: "Whispering Woods", levelRange: [26, 49], monsterPool: [MONSTERS.WOLF, MONSTERS.BAT, MONSTERS.GIANT_SPIDER], coords: {top: '83%', left: '50%'}, icon: 'images/icons/whispering_woods.png' },
                     // --- Sub-Zone: Guardian's Knoll (Boss Lvl 50) ---
-                    "guardian_knoll": { name: "Guardian's Knoll", levelRange: [50, 50], monsterPool: [MONSTERS.DUNGEON_GUARDIAN], coords: {top: '48%', left: '50%'}, isBoss: true, icon: 'images/icons/guardian_knoll.png' },
+                    "guardian_knoll": { name: "Guardian's Knoll", levelRange: [50, 50], monsterPool: [MONSTERS.DUNGEON_GUARDIAN], coords: {top: '44%', left: '50%'}, isBoss: true, icon: 'images/icons/guardian_knoll.png' },
                     // --- Sub-Zone: Sun-dappled Hills (Levels 51-74) ---
                     "sun_dappled_hills": { name: "Sun-dappled Hills", levelRange: [51, 74], monsterPool: [MONSTERS.TREANT_SPROUT, MONSTERS.HOBGOBLIN, MONSTERS.FOREST_SPRITE], coords: {top: '15%', left: '50%'}, icon: 'images/icons/sun_dappled_hills.png' },
                     // --- Sub-Zone: Royal Vanguard (Boss Lvl 75) ---
                     "royal_vanguard": { name: "Royal Vanguard", levelRange: [75, 75], monsterPool: [MONSTERS.ROYAL_GRIFFIN], coords: {top: '35%', left: '68%'}, isBoss: true, icon: 'images/icons/royal_vanguard.png' },
                     // --- Sub-Zone: Royal Hunting Grounds (Levels 76-99) ---
-                    "royal_hunting_grounds": { name: "Royal Hunting Grounds", levelRange: [76, 99], monsterPool: [MONSTERS.GRIFFIN_CHICK, MONSTERS.HOBGOBLIN, MONSTERS.FOREST_SPRITE], coords: {top: '60%', left: '88%'}, icon: 'images/icons/royal_hunting_grounds.png' },
+                    "royal_hunting_grounds": { name: "Royal Hunting Grounds", levelRange: [76, 99], monsterPool: [MONSTERS.GRIFFIN_CHICK, MONSTERS.HOBGOBLIN, MONSTERS.FOREST_SPRITE], coords: {top: '50%', left: '88%'}, icon: 'images/icons/royal_hunting_grounds.png' },
                     // --- Sub-Zone: King's Castle (Boss Lvl 100) ---
-                    "kings_castle": { name: "King's Castle", levelRange: [100, 100], monsterPool: [MONSTERS.KING_OF_SLIMES], coords: {top: '20%', left: '85%'}, isBoss: true, icon: 'images/icons/kings_castle.png' }
+                    "kings_castle": { name: "King's Castle", levelRange: [100, 100], monsterPool: [MONSTERS.KING_OF_SLIMES], coords: {top: '20%', left: '82%'}, isBoss: true, icon: 'images/icons/kings_castle.png' }
                 }
             },
             "orc_volcano": {
@@ -43,38 +43,38 @@ export const REALMS = [
                     // --- Sub-Zone: Ashfall Plains (Levels 101-124) ---
                     "ashfall_plains": { name: "Ashfall Plains", levelRange: [101, 124], monsterPool: [MONSTERS.ORC, MONSTERS.MAGMA_SLIME], coords: {top: '85%', left: '10%'}, icon: 'images/icons/ashfall_plains.png' },
                     // --- Sub-Zone: Orc Watchtower (Boss Lvl 125) ---
-                    "orc_watchtower": { name: "Orc Watchtower", levelRange: [125, 125], monsterPool: [MONSTERS.GIANT_ORC], coords: {top: '80%', left: '50%'}, isBoss: true, icon: 'images/icons/orc_watchtower.png' },
+                    "orc_watchtower": { name: "Orc Watchtower", levelRange: [125, 125], monsterPool: [MONSTERS.GIANT_ORC], coords: {top: '80%', left: '44%'}, isBoss: true, icon: 'images/icons/orc_watchtower.png' },
                     // --- Sub-Zone: Scorched Path (Levels 126-149) ---
                     "scorched_path": { name: "Scorched Path", levelRange: [126, 149], monsterPool: [MONSTERS.ORC_SHAMAN, MONSTERS.LAVA_TURTLE], coords: {top: '80%', left: '80%'}, icon: 'images/icons/scorched_path.png' },
                     // --- Sub-Zone: Molten Heart (Boss Lvl 150) ---
-                    "molten_heart": { name: "Molten Heart", levelRange: [150, 150], monsterPool: [MONSTERS.MOLTEN_GUARDIAN], coords: {top: '50%', left: '75%'}, isBoss: true, icon: 'images/icons/molten_heart.png' },
+                    "molten_heart": { name: "Molten Heart", levelRange: [150, 150], monsterPool: [MONSTERS.MOLTEN_GUARDIAN], coords: {top: '55%', left: '60%'}, isBoss: true, icon: 'images/icons/molten_heart.png' },
                     // --- Sub-Zone: Charred Forest (Levels 151-174) ---
-                    "charred_forest": { name: "Charred Forest", levelRange: [151, 174], monsterPool: [MONSTERS.FIRE_ELEMENTAL, MONSTERS.SALAMANDER], coords: {top: '50%', left: '45%'}, icon: 'images/icons/charred_forest.png' },
+                    "charred_forest": { name: "Charred Forest", levelRange: [151, 174], monsterPool: [MONSTERS.FIRE_ELEMENTAL, MONSTERS.SALAMANDER], coords: {top: '57%', left: '26%'}, icon: 'images/icons/charred_forest.png' },
                     // --- Sub-Zone: Chieftain's Camp (Boss Lvl 175) ---
-                    "chieftains_camp": { name: "Chieftain's Camp", levelRange: [175, 175], monsterPool: [MONSTERS.OBSIDIAN_GOLEM], coords: {top: '50%', left: '15%'}, isBoss: true, icon: 'images/icons/chieftains_camp.png' },
+                    "chieftains_camp": { name: "Chieftain's Camp", levelRange: [175, 175], monsterPool: [MONSTERS.OBSIDIAN_GOLEM], coords: {top: '25%', left: '18%'}, isBoss: true, icon: 'images/icons/chieftains_camp.png' },
                     // --- Sub-Zone: Volcano's Maw (Levels 176-199) ---
-                    "volcanos_maw": { name: "Volcano's Maw", levelRange: [176, 199], monsterPool: [MONSTERS.ORC_BERSERKER, MONSTERS.FIRE_ELEMENTAL], coords: {top: '20%', left: '30%'}, icon: 'images/icons/volcanos_maw.png' },
+                    "volcanos_maw": { name: "Volcano's Maw", levelRange: [176, 199], monsterPool: [MONSTERS.ORC_BERSERKER, MONSTERS.FIRE_ELEMENTAL], coords: {top: '23%', left: '40%'}, icon: 'images/icons/volcanos_maw.png' },
                     // --- Sub-Zone: Volcano Peak (Boss Lvl 200) ---
-                    "volcano_peak": { name: "Volcano Peak", levelRange: [200, 200], monsterPool: [MONSTERS.RULER_OF_ORCS], coords: {top: '15%', left: '70%'}, isBoss: true, icon: 'images/icons/volcano_peak.png' }
+                    "volcano_peak": { name: "Volcano Peak", levelRange: [200, 200], monsterPool: [MONSTERS.RULER_OF_ORCS], coords: {top: '18%', left: '66%'}, isBoss: true, icon: 'images/icons/volcano_peak.png' }
                 }
             },
             "undead_desert": {
                 name: "Undead Desert",
                 mapImage: "images/backgrounds/sub_zone/map_desert_zoomed.png",
                 monsterAreaBg: "images/backgrounds/bg_undead_desert.png",
-                coords: { top: '70%', left: '75%' },
+                coords: { top: '68%', left: '73.5%' },
                 icon: 'images/icons/undead_desert.png',
                 subZones: {
                     // --- Sub-Zone: Lost Tombs (Levels 201-224) ---
                     "lost_tombs": { name: "Lost Tombs", levelRange: [201, 224], monsterPool: [MONSTERS.SKELETON, MONSTERS.VULTURE], coords: {top: '85%', left: '15%'}, icon: 'images/icons/lost_tombs.png' },
                     // --- Sub-Zone: Mummy's Crypt (Boss Lvl 225) ---
-                    "mummy_crypt": { name: "Mummy's Crypt", levelRange: [225, 225], monsterPool: [MONSTERS.CURSED_MUMMY], coords: {top: '85%', left: '45%'}, isBoss: true, icon: 'images/icons/mummy_crypt.png' },
+                    "mummy_crypt": { name: "Mummy's Crypt", levelRange: [225, 225], monsterPool: [MONSTERS.CURSED_MUMMY], coords: {top: '85%', left: '38%'}, isBoss: true, icon: 'images/icons/mummy_crypt.png' },
                     // --- Sub-Zone: Shifting Sands (Levels 226-249) ---
                     "shifting_sands": { name: "Shifting Sands", levelRange: [226, 249], monsterPool: [MONSTERS.ZOMBIE, MONSTERS.GHOUL], coords: {top: '85%', left: '75%'}, icon: 'images/icons/shifting_sands.png' },
                     // --- Sub-Zone: Cursed Pyramid (Boss Lvl 250) ---
-                    "cursed_pyramid": { name: "Cursed Pyramid", levelRange: [250, 250], monsterPool: [MONSTERS.TOMB_ROBBER], coords: {top: '50%', left: '80%'}, isBoss: true, icon: 'images/icons/cursed_pyramid.png' },
+                    "cursed_pyramid": { name: "Cursed Pyramid", levelRange: [250, 250], monsterPool: [MONSTERS.TOMB_ROBBER], coords: {top: '48%', left: '77%'}, isBoss: true, icon: 'images/icons/cursed_pyramid.png' },
                     // --- Sub-Zone: Scorpion Nest (Levels 251-274) ---
-                    "scorpion_nest": { name: "Scorpion Nest", levelRange: [251, 274], monsterPool: [MONSTERS.GIANT_SCORPION, MONSTERS.SKELETAL_ARCHER], coords: {top: '50%', left: '50%'}, icon: 'images/icons/scorpion_nest.png' },
+                    "scorpion_nest": { name: "Scorpion Nest", levelRange: [251, 274], monsterPool: [MONSTERS.GIANT_SCORPION, MONSTERS.SKELETAL_ARCHER], coords: {top: '40%', left: '50%'}, icon: 'images/icons/scorpion_nest.png' },
                     // --- Sub-Zone: Tomb of the Guardian (Boss Lvl 275) ---
                     "tomb_of_the_guardian": { name: "Tomb of the Guardian", levelRange: [275, 275], monsterPool: [MONSTERS.SAND_WRAITH], coords: {top: '50%', left: '20%'}, isBoss: true, icon: 'images/icons/tomb_of_the_guardian.png' },
                     // --- Sub-Zone: Endless Dunes (Levels 276-299) ---
@@ -87,25 +87,25 @@ export const REALMS = [
                 name: "Final Dungeon",
                 mapImage: "images/backgrounds/sub_zone/map_dungeon_zoomed.png",
                 monsterAreaBg: "images/backgrounds/bg_final_dungeon.png",
-                coords: { top: '38%', left: '78%' },
+                coords: { top: '34%', left: '72%' },
                 icon: 'images/icons/final_dungeon.png',
                 subZones: {
                     // --- Sub-Zone: The Gatehouse (Levels 301-324) ---
                     "gatehouse": { name: "The Gatehouse", levelRange: [301, 324], monsterPool: [MONSTERS.GARGOYLE, MONSTERS.IMP], coords: {top: '80%', left: '20%'}, icon: 'images/icons/gatehouse.png' },
                     // --- Sub-Zone: Demon Sentry (Boss Lvl 325) ---
-                    "demon_sentry": { name: "Demon Sentry", levelRange: [325, 325], monsterPool: [MONSTERS.DEMON_SENTRY_BOSS], coords: {top: '80%', left: '50%'}, isBoss: true, icon: 'images/icons/demon_sentry.png' },
+                    "demon_sentry": { name: "Demon Sentry", levelRange: [325, 325], monsterPool: [MONSTERS.DEMON_SENTRY_BOSS], coords: {top: '52%', left: '40%'}, isBoss: true, icon: 'images/icons/demon_sentry.png' },
                     // --- Sub-Zone: Halls of Damnation (Levels 326-349) ---
-                    "halls_of_damnation": { name: "Halls of Damnation", levelRange: [326, 349], monsterPool: [MONSTERS.HELLHOUND, MONSTERS.CULTIST], coords: {top: '80%', left: '80%'}, icon: 'images/icons/halls_of_damnation.png' },
+                    "halls_of_damnation": { name: "Halls of Damnation", levelRange: [326, 349], monsterPool: [MONSTERS.HELLHOUND, MONSTERS.CULTIST], coords: {top: '58%', left: '55%'}, icon: 'images/icons/halls_of_damnation.png' },
                     // --- Sub-Zone: The Hellforge (Boss Lvl 350) ---
                     "the_hellforge": { name: "The Hellforge", levelRange: [350, 350], monsterPool: [MONSTERS.HELLFORGE_GUARDIAN], coords: {top: '50%', left: '75%'}, isBoss: true, icon: 'images/icons/the_hellforge.png' },
                     // --- Sub-Zone: Brimstone Corridors (Levels 351-374) ---
-                    "brimstone_corridors": { name: "Brimstone Corridors", levelRange: [351, 374], monsterPool: [MONSTERS.TOXIC_SULPHUR_GAS, MONSTERS.SUCCUBUS], coords: {top: '50%', left: '45%'}, icon: 'images/icons/brimstone_corridors.png' },
+                    "brimstone_corridors": { name: "Brimstone Corridors", levelRange: [351, 374], monsterPool: [MONSTERS.TOXIC_SULPHUR_GAS, MONSTERS.SUCCUBUS], coords: {top: '30%', left: '75%'}, icon: 'images/icons/brimstone_corridors.png' },
                     // --- Sub-Zone: Pit Lord's Arena (Boss Lvl 375) ---
-                    "pit_lord_arena": { name: "Pit Lord's Arena", levelRange: [375, 375], monsterPool: [MONSTERS.PIT_FIEND], coords: {top: '50%', left: '15%'}, isBoss: true, icon: 'images/icons/pit_lord_arena.png' },
+                    "pit_lord_arena": { name: "Pit Lord's Arena", levelRange: [375, 375], monsterPool: [MONSTERS.PIT_FIEND], coords: {top: '20%', left: '50%'}, isBoss: true, icon: 'images/icons/pit_lord_arena.png' },
                     // --- Sub-Zone: Throne Approach (Levels 376-399) ---
-                    "throne_approach": { name: "Throne Approach", levelRange: [376, 399], monsterPool: [MONSTERS.CHAOS_DEMON, MONSTERS.PIT_FIEND], coords: {top: '20%', left: '30%'}, icon: 'images/icons/throne_approach.png' },
+                    "throne_approach": { name: "Throne Approach", levelRange: [376, 399], monsterPool: [MONSTERS.CHAOS_DEMON, MONSTERS.PIT_FIEND], coords: {top: '40%', left: '25%'}, icon: 'images/icons/throne_approach.png' },
                     // --- Sub-Zone: Archdemon's Lair (Boss Lvl 400) ---
-                    "archdemon_lair": { name: "Archdemon's Lair", levelRange: [400, 400], monsterPool: [MONSTERS.ARCHDEMON_OVERLORD], coords: {top: '15%', left: '70%'}, isBoss: true, icon: 'images/icons/archdemon_lair.png' }
+                    "archdemon_lair": { name: "Archdemon's Lair", levelRange: [400, 400], monsterPool: [MONSTERS.ARCHDEMON_OVERLORD], coords: {top: '85%', left: '45%'}, isBoss: true, icon: 'images/icons/archdemon_lair.png' }
                 }
             }
         }
